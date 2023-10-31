3f81aa84139fb72fee6bd040ce0daa4920566fea
anshulmodh <anshulmodh@gmail.com>
2023-10-31 17:18:31 -0400

    initial pipeline commit

---
+++ data_ingestion/app.py
@@ -0,0 +1,20 @@
+from prometheus_client import start_http_server, Gauge
+import random
+import time
+
+temperature_gauge = Gauge('temperature_celsius', 'Current temperature in Celsius')
+
+def generate_temperature_data():
+    while True:
+        # Simulate temperature data (replace this with your own data source)
+        temperature = random.uniform(20.0, 45.0)
+        temperature_gauge.set(temperature)
+        print(f'Temp set: {temperature}')
+        time.sleep(5)
+
+if __name__ == '__main__':
+    # Start an HTTP server to expose the metrics
+    start_http_server(8070)
+    print("Exporter is running on :8070/metrics")
+
+    generate_temperature_data()
---
+++ mission_api/app.py
@@ -0,0 +1,29 @@
+import prometheus_api_client
+from flask import Flask, request
+
+import time
+
+app = Flask(__name__)
+
+@app.route('/alert', methods=['POST'])
+def receive_alert():
+    data = request.json  # Get the JSON data from the request
+    print(data)  # Print the data to the terminal
+    return 'OK'
+
+def main():
+    prometheus_url = "http://mission_data:9090"
+    query = 'up'
+    
+    # Create a Prometheus API client
+    prometheus_api = prometheus_api_client.PrometheusConnect(url=prometheus_url)
+    
+    # Query Prometheus for data
+    result = prometheus_api.custom_query(query)
+    
+    print(f"Prometheus Query: {query}")
+    for item in result:
+        print(item['metric'], item['value'])
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
