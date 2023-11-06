3b4e4b60ff4675db6f3c34c7169a27d7cdc52ea1
anshulmodh <anshulmodh@gmail.com>
2023-11-06 11:10:45 -0500

    integrate with gds

--- data_ingestion/app.py
+++ data_ingestion/app.py
@@ -1,11 +1,30 @@
-from datetime import datetime
-
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+from datetime import datetime
 
+import socket
 import os
 import random
 import time
+import json
+
+# Define the server address and port
+server_address = ('0.0.0.0', 8070)
+
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address and port
+server_socket.bind(server_address)
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+print(f"Waiting for a connection on {server_address}")
+
+# Accept a connection
+client_socket, client_address = server_socket.accept()
+print(f"Accepted connection from {client_address}")
 
 token = os.getenv('DOCKER_INFLUXDB_INIT_ADMIN_TOKEN')
 org = os.getenv('DOCKER_INFLUXDB_INIT_ORG')
@@ -13,6 +32,33 @@ bucket = os.getenv('DOCKER_INFLUXDB_INIT_BUCKET')
 
 client = InfluxDBClient(url="http://mission_data:8086", token=token)
 write_api = client.write_api(write_options=SYNCHRONOUS)
+
+def start_server():
+	try:
+		while True:
+			# Receive telemetry data from the client
+			telemetry_data = client_socket.recv(1024)
+			if not telemetry_data:
+				print('Not Found')
+				continue
+
+			json_data = client_socket.recv(1024).decode('utf-8')
+			received_data = json.loads(json_data)
+			
+			print(f"Received telemetry data: {received_data.keys()}")
+			point = Point("temps")\
+				.field("celsius", received_data['TempC'])\
+				.time(datetime.utcnow(), WritePrecision.NS)
+
+			print(received_data['TempC'])
+			write_api.write(bucket, org, point)
+	except KeyboardInterrupt:
+		print("Client terminated.")
+
+	# Clean up
+	print('closing')
+	client_socket.close()
+	server_socket.close()
 
 def generate_temperature_data():
 	while True:
@@ -27,4 +73,5 @@ def generate_temperature_data():
 		time.sleep(5)
 
 if __name__ == '__main__':
-    generate_temperature_data()
+	start_server()
+    # generate_temperature_data()
---
+++ ipc/mdap.py
@@ -0,0 +1,72 @@
+import socket
+import json
+from typing import cast
+
+import IrisBackendv3 as IB3
+import IrisBackendv3.ipc as ipc
+from IrisBackendv3.codec.payload import TelemetryPayload
+from IrisBackendv3.ipc.messages import DownlinkedPayloadsMessage
+
+# Define the server address and port (must match the values used in telemetry server)
+server_address = ('127.0.0.1', 8070)
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect(server_address)
+print("Connection with the server established successfully.")
+
+
+IB3.init_from_latest()
+
+app = ipc.IpcAppHelper("FleurDLPayloadListener")
+manager = ipc.IpcAppManagerSync(socket_specs={
+    'sub': ipc.SocketSpec(
+        sock_type=ipc.SocketType.SUBSCRIBER,
+        port=ipc.Port.TRANSCEIVER_SUB,
+        topics=[ipc.Topic.DL_PAYLOADS]
+    )
+})
+
+if __name__ == "__main__":
+    app.setLogLevel('VERBOSE')
+
+    telemetryCounter = dict()
+    eventCounter = dict()
+
+    # while True:
+    for _ in range(500):  # read 50 packets worth of data then stop
+        ipc_payload = manager.read('sub')
+        try:
+            msg = ipc.guard_msg(ipc_payload.message,
+                                DownlinkedPayloadsMessage)
+            payloads = msg.content.payloads
+        except Exception as e:
+            app.logger.error(
+                f"Failed to decode IPC message `{msg}` "
+                f"of `{ipc_payload=}` b/c: `{e}`."
+            )
+
+        # Print out all Telemetry we received:
+        for telemetry in payloads[TelemetryPayload]:
+            telemetry = cast(TelemetryPayload, telemetry)
+
+            # Fish out a special telemetry channel we care about:
+            if (telemetry.module.name, telemetry.channel.name) == ('WatchdogHeartbeatTvac', 'AdcTempKelvin'):
+                app.logger.notice(f"BATTERY TEMP IS: {telemetry.data - 273.15}ºC")
+
+                data_to_send = {
+                    'TempC': telemetry.data - 273.15
+                }
+                json_data = json.dumps(data_to_send)
+                # Send the telemetry data to FLEUR backend server.
+                client_socket.send(json_data.encode('utf-8'))
+            else:
+                app.logger.verbose(f"GOT: {telemetry}")
+
+            
+
+# Clean up
+print('closing')
+client_socket.close()
