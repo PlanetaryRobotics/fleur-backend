3d65654a8962488c5b95895a1ee59e4e917db6ca
anshulmodh <anshulmodh@gmail.com>
2023-11-06 00:50:09 -0500

    iterate to influxdb

--- data_ingestion/app.py
+++ data_ingestion/app.py
@@ -1,21 +1,30 @@
-from prometheus_client import start_http_server, Gauge
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+import os
 import random
 import time
 
-temperature_gauge = Gauge('temperature_celsius', 'Current temperature in Celsius')
+token = os.getenv('DOCKER_INFLUXDB_INIT_ADMIN_TOKEN')
+org = os.getenv('DOCKER_INFLUXDB_INIT_ORG')
+bucket = os.getenv('DOCKER_INFLUXDB_INIT_BUCKET')
 
-# This file become the kafka broker.
+client = InfluxDBClient(url="http://mission_data:8086", token=token)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
 def generate_temperature_data():
-    while True:
-        # Simulate temperature data (replace this with your own data source)
-        temperature = random.uniform(20.0, 45.0)
-        temperature_gauge.set(temperature)
-        time.sleep(5)
+	while True:
+		temperature = random.uniform(0.0, 100.0)
 
+		point = Point("temps")\
+			.field("celsius", temperature)\
+			.time(datetime.utcnow(), WritePrecision.NS)
+
+		print(temperature)
+		write_api.write(bucket, org, point)
+		time.sleep(5)
 
 if __name__ == '__main__':
-    # Start an HTTP server to expose the metrics
-    start_http_server(8070)
-    print("Exporter is running on :8070/metrics")
-
     generate_temperature_data()
