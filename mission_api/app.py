648bca1eaf3ddb1deeaaa27f03f2f728739891e4
anshulmodh <anshulmodh@gmail.com>
2023-11-19 16:01:58 -0500

    Add alerts to influx

--- mission_api/app.py
+++ mission_api/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
-from influxdb_client import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from pydantic import BaseModel
 
 import uvicorn
 import os
@@ -10,11 +12,42 @@ influx_url = "http://mission_data:8086"
 token = os.getenv('DOCKER_INFLUXDB_INIT_ADMIN_TOKEN')
 org = os.getenv('DOCKER_INFLUXDB_INIT_ORG')
 client = InfluxDBClient(url=influx_url, token=token)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+class AlertRequest(BaseModel, extra='allow'):
+    _check_id: str
+    _check_name: str
+    _level: str
+    _measurement: str
+    _message: str
+    _notification_endpoint_id: str
+    _notification_endpoint_name: str
+    _notification_rule_id: str
+    _notification_rule_name: str
+    _source_measurement: str
+    _source_timestamp: int
+    _start: str
+    _status_timestamp: int
+    _stop: str
+    _time: str
+    _type: str
+    _version: int
 
 @app.post('/alert')
-async def receive_alert(request: Request):
-    data = await request.json()  # Get the JSON data from the request
-    print(data)  # Print the data to the terminal
+async def receive_alert(request: AlertRequest):
+    try:
+        data = request.dict()
+
+        point = Point(data["_check_name"]) \
+            .field(data["_notification_rule_name"], data["_message"]) \
+            .time(data["_start"], WritePrecision.S) \
+            .tag("measurement_name", data["_source_measurement"]) \
+            .tag("value", data[data["_source_measurement"]])
+        
+        write_api.write("mission_alerts", org, point)
+    except Exception as e:
+        return {'Error': e}
+    
     return 'OK'
         
 if __name__ == "__main__":
