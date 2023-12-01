02615fcd636ae60fac0a10f131341128871a9896
yashsinha <yashsinha7696@gmail.com>
2023-12-01 12:28:33 -0500

    uncommented

--- ipc/mdap.py
+++ ipc/mdap.py
@@ -1,132 +1,132 @@
-# import socket
-# import json
-# from typing import cast
-# import IrisBackendv3 as IB3
-# import IrisBackendv3.ipc as ipc
-# from IrisBackendv3.codec.payload import TelemetryPayload, EventPayload
-# from IrisBackendv3.ipc.messages import DownlinkedPayloadsMessage
-#
-# IB3.init_from_latest()
-#
-# app = ipc.IpcAppHelper("FleurDLPayloadListener")
-# app.setLogLevel('VERBOSE')
-#
-# manager = ipc.IpcAppManagerSync(socket_specs={
-#     'sub': ipc.SocketSpec(
-#         sock_type=ipc.SocketType.SUBSCRIBER,
-#         port=ipc.Port.TRANSCEIVER_SUB,
-#         topics=[ipc.Topic.DL_PAYLOADS]
-#     )
-# })
-#
-#
-# def connect_to_telemetry_server(address: tuple) -> socket.socket:
-#     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client_socket.connect(address)
-#     print("Connection with the telemetry server established successfully.")
-#     return client_socket
-#
-#
-# def telem_to_message(data_to_send, payloads):
-#     for telemetry in payloads[TelemetryPayload]:
-#         telemetry = cast(TelemetryPayload, telemetry)
-#
-#         message = {
-#             "value": telemetry.data,
-#             "timestamp": telemetry.timestamp,
-#             "bucket": "mission_data"
-#         }
-#
-#         key = f"{telemetry.module.name}-{telemetry.channel.name}"
-#
-#         if key in data_to_send:
-#             data_to_send[key].append(message)
-#         else:
-#             telemetry_list = [message]
-#             data_to_send[key] = telemetry_list
-#
-#         if key == "WatchdogHeartbeatTvac-AdcTempKelvin":
-#             key = "TempC"
-#
-#             # Convert the temperature from Kelvin to Celsius.
-#             temperature = telemetry.data - 273.15
-#             app.logger.notice(f"BATTERY TEMP IS: {temperature}ºC")
-#
-#             message = {
-#                     "value": temperature,
-#                     "timestamp": telemetry.timestamp,
-#                     "bucket": "mission_data"
-#             }
-#             if key in data_to_send:
-#                 data_to_send[key].append(message)
-#             else:
-#                 telemetry_list = [message]
-#                 data_to_send[key] = telemetry_list
-#
-#     return data_to_send
-#
-#
-# def events_to_message(data_to_send, payloads):
-#     for event in payloads[EventPayload]:
-#         event = cast(EventPayload, event)
-#
-#         key = f"{event.module.name}-{event.event.name}"
-#         message = {
-#             "value": event.formatted_string,
-#             "timestamp": event.timestamp,
-#             "bucket": "mission_events"
-#         }
-#
-#         if key in data_to_send:
-#             data_to_send[key].append(message)
-#         else:
-#             event_list = [message]
-#             data_to_send[key] = event_list
-#
-#     return data_to_send
-#
-#
-# def process_ipc_payload(ipc_payload):
-#     try:
-#         msg = ipc.guard_msg(ipc_payload.message, DownlinkedPayloadsMessage)
-#         payloads = msg.content.payloads
-#     except Exception as e:
-#         app.logger.error(
-#             f"Failed to decode IPC message `{msg}` "
-#             f"of `{ipc_payload=}` b/c: `{e}`."
-#         )
-#         return None
-#
-#     data_to_send = {}
-#
-#     data_to_send = telem_to_message(data_to_send, payloads)
-#     data_to_send = events_to_message(data_to_send, payloads)
-#
-#     return data_to_send
-#
-#
-# def send_data_to_backend(sock, data):
-#     json_data = json.dumps(data)
-#     sock.send(json_data.encode('utf-8'))
-#
-#
-# def main():
-#     server_address = ('127.0.0.1', 8070)
-#     client_socket = connect_to_telemetry_server(server_address)
-#
-#     try:
-#         while True:
-#             ipc_payload = manager.read('sub')
-#             data_to_send = process_ipc_payload(ipc_payload)
-#
-#             if data_to_send:
-#                 print(data_to_send)
-#                 send_data_to_backend(client_socket, data_to_send)
-#
-#     except KeyboardInterrupt:
-#         print('Closing...')
-#         client_socket.close()
-#
-#
-# if __name__ == "__main__":
-#     main()
+import socket
+import json
+from typing import cast
+import IrisBackendv3 as IB3
+import IrisBackendv3.ipc as ipc
+from IrisBackendv3.codec.payload import TelemetryPayload, EventPayload
+from IrisBackendv3.ipc.messages import DownlinkedPayloadsMessage
+
+IB3.init_from_latest()
+
+app = ipc.IpcAppHelper("FleurDLPayloadListener")
+app.setLogLevel('VERBOSE')
+
+manager = ipc.IpcAppManagerSync(socket_specs={
+    'sub': ipc.SocketSpec(
+        sock_type=ipc.SocketType.SUBSCRIBER,
+        port=ipc.Port.TRANSCEIVER_SUB,
+        topics=[ipc.Topic.DL_PAYLOADS]
+    )
+})
+
+
+def connect_to_telemetry_server(address: tuple) -> socket.socket:
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(address)
+    print("Connection with the telemetry server established successfully.")
+    return client_socket
+
+
+def telem_to_message(data_to_send, payloads):
+    for telemetry in payloads[TelemetryPayload]:
+        telemetry = cast(TelemetryPayload, telemetry)
+
+        message = {
+            "value": telemetry.data,
+            "timestamp": telemetry.timestamp,
+            "bucket": "mission_data"
+        }
+
+        key = f"{telemetry.module.name}-{telemetry.channel.name}"
+
+        if key in data_to_send:
+            data_to_send[key].append(message)
+        else:
+            telemetry_list = [message]
+            data_to_send[key] = telemetry_list
+
+        if key == "WatchdogHeartbeatTvac-AdcTempKelvin":
+            key = "TempC"
+
+            # Convert the temperature from Kelvin to Celsius.
+            temperature = telemetry.data - 273.15
+            app.logger.notice(f"BATTERY TEMP IS: {temperature}ºC")
+
+            message = {
+                    "value": temperature,
+                    "timestamp": telemetry.timestamp,
+                    "bucket": "mission_data"
+            }
+            if key in data_to_send:
+                data_to_send[key].append(message)
+            else:
+                telemetry_list = [message]
+                data_to_send[key] = telemetry_list
+
+    return data_to_send
+
+
+def events_to_message(data_to_send, payloads):
+    for event in payloads[EventPayload]:
+        event = cast(EventPayload, event)
+
+        key = f"{event.module.name}-{event.event.name}"
+        message = {
+            "value": event.formatted_string,
+            "timestamp": event.timestamp,
+            "bucket": "mission_events"
+        }
+
+        if key in data_to_send:
+            data_to_send[key].append(message)
+        else:
+            event_list = [message]
+            data_to_send[key] = event_list
+
+    return data_to_send
+
+
+def process_ipc_payload(ipc_payload):
+    try:
+        msg = ipc.guard_msg(ipc_payload.message, DownlinkedPayloadsMessage)
+        payloads = msg.content.payloads
+    except Exception as e:
+        app.logger.error(
+            f"Failed to decode IPC message `{msg}` "
+            f"of `{ipc_payload=}` b/c: `{e}`."
+        )
+        return None
+
+    data_to_send = {}
+
+    data_to_send = telem_to_message(data_to_send, payloads)
+    data_to_send = events_to_message(data_to_send, payloads)
+
+    return data_to_send
+
+
+def send_data_to_backend(sock, data):
+    json_data = json.dumps(data)
+    sock.send(json_data.encode('utf-8'))
+
+
+def main():
+    server_address = ('127.0.0.1', 8070)
+    client_socket = connect_to_telemetry_server(server_address)
+
+    try:
+        while True:
+            ipc_payload = manager.read('sub')
+            data_to_send = process_ipc_payload(ipc_payload)
+
+            if data_to_send:
+                print(data_to_send)
+                send_data_to_backend(client_socket, data_to_send)
+
+    except KeyboardInterrupt:
+        print('Closing...')
+        client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
